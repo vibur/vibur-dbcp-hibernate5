6acf1301c6771d1059d8aa236fe17681d526e7b3
simeonmalchev <simeon.malchev@gmail.com>
2015-10-21 23:38:28 +1100

    year update

--- src/main/java/org/vibur/dbcp/integration/ViburDBCPConnectionProvider.java
+++ src/main/java/org/vibur/dbcp/integration/ViburDBCPConnectionProvider.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013 Simeon Malchev
+ * Copyright 2015 Simeon Malchev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
